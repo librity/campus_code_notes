cce1fca64bcb9e43bd381475a45b7eb8c511c4e2
Luis Geniole <luis.geniole@gmail.com>
2020-04-07 01:19:43 -0300

    Third feature finished.

--- car_rental_app/app/controllers/manufacturers_controller.rb
+++ car_rental_app/app/controllers/manufacturers_controller.rb
@@ -8,4 +8,25 @@ class ManufacturersController < ApplicationController
   def show
     @manufacturer = Manufacturer.find(params[:id])
   end
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+    if @manufacturer.save
+      flash[:success] = 'Fabricante criado com sucesso'
+      redirect_to @manufacturer
+    else
+      flash.now[:error] = 'Algo deu errado'
+      render :new
+    end
+  end
+
+  private
+
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name)
+  end
 end
